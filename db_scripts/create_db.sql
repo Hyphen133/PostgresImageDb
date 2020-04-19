70344ef37c1d72bc0f136efe00a5dce66dbbccfd
wmosienski <w.mosienski@gmail.com>
2020-04-19 12:51:23 -0400

    create db

--- db_scripts/create_db.sql
+++ db_scripts/create_db.sql
@@ -10,3 +10,46 @@ CREATE TABLE public."Image"
 
 ALTER TABLE public."Image"
     OWNER to postgres;
+
+CREATE TABLE public."Tag"
+(
+    id bigint NOT NULL,
+    name character varying[] NOT NULL,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE public."Tag"
+    OWNER to postgres;
+
+CREATE TABLE public."ImageTag"
+(
+    id bigint NOT NULL,
+    "imageId" bigint NOT NULL,
+    "tagId" bigint NOT NULL,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE public."ImageTag"
+    OWNER to postgres;
+
+CREATE TABLE public."Dataset"
+(
+    id bigint NOT NULL,
+    name character varying[] NOT NULL,
+    description character varying[] NOT NULL,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE public."Dataset"
+    OWNER to postgres;
+
+CREATE TABLE public."ImageDataset"
+(
+    id bigint NOT NULL,
+    "datasetId" bigint NOT NULL,
+    "imageId" bigint NOT NULL,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE public."ImageDataset"
+    OWNER to postgres;
