0752dfebc5c45ec8b60fc331381f04bf65604106
wmosienski <w.mosienski@gmail.com>
2020-04-28 03:38:55 -0400

    export images

--- client/fill_database.py
+++ client/fill_database.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 from client.query_base import QueryBase
 from client.write_image import ImageWriteQuery
@@ -9,13 +10,12 @@ class FillDataQuery(QueryBase):
         super().__init__()
 
     def execute_query(self):
-        images_size = 10
 
         image_ids = []
-        for i in range(images_size):
+        for filename in os.listdir('test_images'):
             id = str(random.getrandbits(60))
             image_ids.append(id)
-            ImageWriteQuery.create_query(id, "test_images/lenna.png", ".png").execute()
+            ImageWriteQuery.create_query(id, "test_images/" + filename, filename.split(".")[1]).execute()
 
 
         tag_ids = []
@@ -76,7 +76,7 @@ class FillDataQuery(QueryBase):
             range_left = random.randint(0, len(image_ids)-1)
             range_size = random.randint(1, len(image_ids)-range_left)
             for image_idx in range(range_size):
-                image_idx = tag_idx + range_left
+                image_idx = image_idx + range_left
                 id = str(random.getrandbits(60))
                 self.cursor.execute("""
                             INSERT INTO public."ImageDataset"(id,"imageId", "datasetId")
